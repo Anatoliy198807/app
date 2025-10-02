b980e836fcfb6c904bd22a00b53012e2b2f5a764
Anatoliy198807 <fakurez@mail.ru>
2025-10-02 03:36:41 -1100

    Добавлены: базовая страница, детали постов, ссылки на посты

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index_view
+from .views import index_view, post_list_view, post_detail_view
 
 urlpatterns = [
-    path("", index_view)
+    path("", index_view),
+    path("all-posts/", post_list_view, name ="post_list"),
+    path("posts/<int:post_id>/", post_detail_view, name = "post_detail")
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Post
 
 # Create your views here.
 
 def index_view(request):
-    return HttpResponse("Главная страница")
+    return render(request, 'index.html') 
+    #return HttpResponse("Главная страница")
     
-    
+def post_list_view(request):
+    posts = Post.objects.all()
+
+    return render(request, 'post_list.html', {'posts':posts})    
+
+def post_detail_view(request, post_id):
+    #post = Post.objects.get(id=post_id)
+    post =get_object_or_404(Post, id=post_id)
+    return render(request, 'post_detail.html', {'post':post})
